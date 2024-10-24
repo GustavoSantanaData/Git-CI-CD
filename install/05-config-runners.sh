433be9ae4d79dd41d62d649ef96fb092d65d5e0b
Gustavo Santana <gustavo.breno57@gmail.com>
2024-10-24 11:54:45 -0300

    Update 05-config-runners.sh

--- install/05-config-runners.sh
+++ install/05-config-runners.sh
@@ -3,7 +3,7 @@
 # Ingressar o Runner em modo shell no GitLab
 # Se estiver utilizando gitlab.com ou certificado, altere para HTTPS://
 sudo gitlab-runner register -n \
-  --url http://172.17.0.1:8080 \
+  --url http://<seu-ip>:8080 \
   --registration-token GR1348941w5eCWTWhMTxorW_RB4Qu \
   --executor shell \
   --description "Runner Shell"
@@ -21,9 +21,9 @@ docker run -dit \
 # Se estiver utilizando gitlab.com ou certificado, altere para HTTPS://
 docker exec -it runner-docker \
 gitlab-runner register -n \
-  --url http://172.17.0.1:8080/ \
+  --url http://<seu-ip>:8080/ \
   --registration-token GR1348941w5eCWTWhMTxorW_RB4Qu \
-  --clone-url http://172.17.0.1:8080/ \
+  --clone-url http://<seu-ip>:8080/ \
   --executor docker \
   --docker-image "docker:latest" \
   --docker-privileged
